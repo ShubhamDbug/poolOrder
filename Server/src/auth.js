8296083a2489b5b254a4354315afd9926d795afb
shubham_247_991 <141959332+shubham497sharma@users.noreply.github.com>
2025-08-17 14:56:59 +0530

    BackendJob

--- Server/src/auth.js
+++ Server/src/auth.js
@@ -2,7 +2,7 @@
 // Verifies Firebase ID tokens and attaches req.user = { uid, displayName }
 
 import './firebase-init.js'; // ensure Firebase Admin is initialized
-import admin from 'firebase-admin';
+import admin, { auth } from 'firebase-admin';
 
 /**
  * verifyAuth
@@ -14,7 +14,7 @@ import admin from 'firebase-admin';
  */
 export async function verifyAuth(req, _res, next) {
   const authHeader = req.headers.authorization || '';
-
+  console.log(authHeader) ;
   if (!authHeader.startsWith('Bearer ')) {
     req.user = { uid: 'anon' };
     return next();
